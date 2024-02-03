7bf79f953acce6be3736b5aa27b2e6382847cbc6
Bastien SERPRIX <bserprix@webqam.fr>
2024-02-03 17:11:55 +0100

    Fix quotes handle for csv import

--- src/to-i18n.ts
+++ src/to-i18n.ts
@@ -35,7 +35,7 @@ export const convertCsvToArray = (csv: string, csvDelimiter: string) => {
     const result: string[][] = []
     const rows = csv.split('\n')
     rows.forEach(r => {
-      result.push(handleCsvDelimiter(r, csvDelimiter))
+      result.push(handleCsvData(r, csvDelimiter))
     })
     return result
   } else {
@@ -43,22 +43,18 @@ export const convertCsvToArray = (csv: string, csvDelimiter: string) => {
   }
 }
 
-export const handleCsvDelimiter = (row: string, csvDelimiter: string): string[] => {
+export const handleCsvData = (row: string, csvDelimiter: string): string[] => {
   const values = row.split(csvDelimiter);
   const fixedValues: string[] = []
-  let mergedValue = ""
   values.forEach(v => {
-    if (v.startsWith('"')) {
-      mergedValue = v.substring(1) + csvDelimiter;
-    } else if (v.endsWith('"')) {
-      mergedValue += v.substring(0, v.length - 1);
-      fixedValues.push(mergedValue)
-      mergedValue = ""
-    } else if (mergedValue.length) {
-      mergedValue += v + csvDelimiter
-    } else {
-      fixedValues.push(v)
+    let mergedValue = v;
+
+    if (v.startsWith('"') && v.endsWith('"')) {
+      mergedValue = v.substring(1, v.length - 1);
+      mergedValue = mergedValue.replace(/""/g, '"');
     }
+
+    fixedValues.push(mergedValue);
   })
   return fixedValues
 }
